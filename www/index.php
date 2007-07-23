bc5386d603b66dea87b851eb7ed90d9f94b9feb6
byandell (Professor ) <byandell@wisc.edu>
2007-07-23 18:22:46 +0000

    modified home page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pda@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,27 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+The web site <a href="http://www.stat.wisc.edu/~yandell/pda">http://www.stat.wisc.edu/~yandell/pda</a> contains free information from
+<A HREF="http://www.stat.wisc.edu/~yandell/">Brian Yandell</A>'s (1997)
+book <A HREF="summary.html">Practical Data Analysis for Designed
+Experiments</A>.  PDA was published in January 1997 by
+<A HREF="http://www.crcpress.com/www/chaphall.htm">Chapman &amp; Hall/CRC Press</A>,
+<A HREF="http://www.crcpress.com/catalog/C6341.htm">ISBN 0-412-06341-7</A>. 
+<ul>
+<li> <A HREF="http://www.stat.wisc.edu/~yandell/pda/summary.html">Book Summary</A>
+<li>	<A HREF="http://www.stat.wisc.edu/~yandell/pda/outline/">Book Outline</A>
+<li> 	<A HREF="http://www.stat.wisc.edu/~yandell/pda/errata.pdf">Book Errata</A>
+<li> <a href="http://www.stat.wisc.edu/~yandell/pda/data">Data Used in book (includes SAS and Splus code)</a>
+<li> Publisher: <A
+HREF="http://www.crcpress.com/www/chaphall.htm">Chapman &amp; Hall/CRC Press</A> (search: yandell)
+     <LI> Book Reviews:
+<UL>
+  <LI> <a href="http://links.jstor.org/sici?sici=0040-1706%28199805%2940%3A2%3C154%3APDAFDE%3E2.0.CO%3B2-L">Grego JM (1998)
+     <CITE>Technometrics 40</CITE>, 154--155.</a>
+<LI> <a href="http://links.jstor.org/sici?sici=0006-341X%28199812%2954%3A4%3C1678%3APDAFDE%3E2.0.CO%3B2-K">Talbot M (1998)
+     <CITE>Biometrics 54</CITE>, 1678.</a>
+</UL>
+</UL>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
